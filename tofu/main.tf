3dbc547f43a4e77418afb1034a4f3d91cb313adc
Helena Luz <99624336+helenaluz@users.noreply.github.com>
2026-06-21 18:06:14 -0300

    Update tofu/main.tf
    Co-authored-by: Carlos Soler <167230536+cesoler@users.noreply.github.com>

--- tofu/main.tf
+++ tofu/main.tf
@@ -385,8 +385,8 @@ resource "azurerm_container_app" "relatorio_service" {
       }
     }
 
-    min_replicas = 1
-    max_replicas = 1
+    min_replicas = 0
+    max_replicas = 3
   }
 
   depends_on = [
